fb152de7dd57d2c4d01c6a9169ff7f52891e96c2
gil <geunyoung_gil@github.com>
2018-07-01 18:53:07 -0400

    get geo info from google api

---
+++ app.js
@@ -0,0 +1,9 @@
+const request = require('request');
+
+// [NOTE] OVER_QUERY_LIMIT: http://links.mead.io/api-fix
+request({
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?&address=young%20st.%20toronto',
+    json: true
+}, (error, response, body) => {
+    console.log(body);
+});
