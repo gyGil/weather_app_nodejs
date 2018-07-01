329d9cbf7397983cc8687428fb148a150ddc596a
gil <geunyoung_gil@github.com>
2018-07-01 19:11:03 -0400

    change consol log

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?&address=young%20st.%20toronto',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body,undefined,2));
 });
